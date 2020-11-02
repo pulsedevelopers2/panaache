5a879f481ef29136d989374e5f7f17d0f1733282
pulsedevelopers2 <71938596+pulsedevelopers2@users.noreply.github.com>
2020-11-02 13:59:46 +0530

    Add files via upload

--- src/endpoint.js
+++ src/endpoint.js
@@ -1,6 +1,12 @@
 const Utils = require('./utilities/utils');
+const Razorpay = require('razorpay')
 
 const utils = new Utils();
+const instance = new Razorpay({
+  key_id:"rzp_test_ysLINZgR8PucKz",
+  key_secret:"RgkMbsYs1CsPAxI5TCxgOugQ"
+})
+
 class Endpoint {
   async getItems(req, res, category) {
     let result = await utils.getItems(category);
@@ -30,5 +36,18 @@ class Endpoint {
     let result = await utils.viewCart(req, email);
     return result;
   }
+
+  async createOrder(){
+    var options = {
+      amount: 50000,  // amount in the smallest currency unit
+      currency: "INR",
+      receipt: "order_rcptid_11"
+    };
+    instance.orders.create(options).then((data)=>{
+      console.log(data);
+      return data;
+    })
+  }
+
 }
 module.exports = Endpoint;
